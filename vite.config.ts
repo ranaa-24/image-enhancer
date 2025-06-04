d4553aecc0042877d7771e05d9faa6b5233f2667
Rana <2004ranadey@gmail.com>
2025-06-04 13:47:57 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || '/image-enhancer',
   server: {
     port: 3000 // change port
   },
